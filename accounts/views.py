c739e92faffc04f337207fe9a6d30b9ebb8c09ac
AbhishekT-12 <abhishekperul@gmail.com>
2026-07-23 12:39:18 +0530

    Day 2:Authentication - register , login ,logout , profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'accounts/register.html', {'form': form})
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html')
--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/accounts/profile/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
